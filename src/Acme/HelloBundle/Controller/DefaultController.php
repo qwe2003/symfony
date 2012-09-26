088f4746529e4ce09730a4592179849908c7e168
qwe2003 <tobias.meyer@meedia.de>
2012-09-26 17:26:40 +0200

    selber erstelltes login

--- src/Acme/HelloBundle/Controller/DefaultController.php
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -3,11 +3,36 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/hello")
+ */
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('AcmeHelloBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    /**
+     * @Route("/login", name="_hello_login")
+     */
+    public function loginAction() {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        return $this->render('AcmeHelloBundle:Default:login.html.twig', array(
+                    // last username entered by the user
+                    'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                    'error' => $error,
+                ));
     }
 }
